0493d3a7c8db5b0ae9817e97ee4a89b4dfb8d55a
Mang Tuấn Vĩ <tuanvimang@gmail.com>
2023-12-26 14:47:44 +0700

    complete the function of friend
    gửi lời mời kết bạn, chấp nhận lời mời kết bạn, hủy lời mời kết bạn. Xử lí nếu đã gửi lời mời thì gửi thông báo đã gửi. Nếu đã là bạn thì báo lỗi. Nếu cũng gửi lời mời cho nhau sẽ thành bạn trong danh sách bạn bè.

--- models/user.js
+++ models/user.js
@@ -2,49 +2,49 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
-const postSchema = new Schema(
-  {
-    id: { type: ObjectId },
-    title: { type: String },
-    image: { type: String },
-    avatar: { type: String },
-    time: { type: String },
-    actionType: { type: String },
-  },
-  {
-    versionKey: false,
-  }
-);
+const friendRequestSchema = new Schema({
+  sender: { type: ObjectId, ref: 'user' },
+  status: { type: String, enum: ['pending', 'accepted', 'rejected'], default: 'pending' },
+});
 
-const storySchema = new Schema(
-  {
-    id: { type: ObjectId }, // Khóa chính
-    title: { type: String },
-    file: { type: String },
-  },
-  {
-    versionKey: false,
-  }
-);
+const postSchema = new Schema({
+  id: { type: ObjectId },
+  title: { type: String },
+  image: { type: String },
+  avatar: { type: String },
+  time: { type: String },
+  actionType: { type: String },
+},
+{
+  versionKey: false,
+});
 
-const userSchema = new Schema(
-  {
-    id: { type: ObjectId },
-    name: { type: String },
-    email: { type: String },
-    password: { type: String },
-    gioitinh: { type: String },
-    ngaysinh: { type: String },
-    avatar: { type: String },
-    anhbia: { type: String },
-    posts: [postSchema],
-    stories: [storySchema], // Mảng story
-    friends: [{ type: ObjectId, ref: 'user' }], // Mảng friends với kiểu ObjectId và tham chiếu đến model 'user'
-  },
-  {
-    versionKey: false,
-  }
-);
+const storySchema = new Schema({
+  id: { type: ObjectId },
+  title: { type: String },
+  file: { type: String },
+},
+{
+  versionKey: false,
+});
+
+const userSchema = new Schema({
+  id: { type: ObjectId },
+  name: { type: String },
+  email: { type: String },
+  password: { type: String },
+  gioitinh: { type: String },
+  ngaysinh: { type: String },
+  avatar: { type: String },
+  anhbia: { type: String },
+  posts: [postSchema],
+  stories: [storySchema],
+  friends: [{ type: ObjectId, ref: 'user' }],
+  friendRequests: [friendRequestSchema], // Mảng lưu trữ lời mời kết bạn
+},
+{
+  versionKey: false,
+});
 
 // Đổi tên model từ "login" thành "user"
 module.exports = mongoose.models.user || mongoose.model("user", userSchema);
--- routes/friend.js
+++ routes/friend.js
@@ -1,36 +1,121 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
-
-router.post('/add-friend', async (req, res) => {
+// Gửi lời mời kết bạn
+router.post('/send-friend-request', async (req, res) => {
   try {
-    const { userID, friendID } = req.body;
+    const { senderID, receiverID } = req.body;
 
-    const user = await User.findById(userID);
-    const friend = await User.findById(friendID);
+    const sender = await User.findById(senderID);
+    const receiver = await User.findById(receiverID);
 
-    if (!user || !friend) {
+    if (!sender || !receiver) {
       return res.status(404).json({ status: 0, message: 'Người dùng không tồn tại' });
     }
 
     // Kiểm tra xem đã là bạn bè hay chưa
-    if (user.friends.includes(friendID) || friend.friends.includes(userID)) {
+    if (sender.friends.includes(receiverID) || receiver.friends.includes(senderID)) {
       return res.status(400).json({ status: 0, message: 'Đã là bạn bè' });
     }
 
-    // Thêm bạn bè vào mảng friends
-    user.friends.push(friendID);
-    friend.friends.push(userID);
+    // Kiểm tra xem đã gửi lời mời chưa
+    const existingRequest = receiver.friendRequests.find(request => String(request.sender) === String(senderID));
+    if (existingRequest) {
+      return res.status(400).json({ status: 0, message: 'Đã gửi lời mời kết bạn' });
+    }
+
+    // Kiểm tra nếu người nhận đã gửi lời mời, thì thêm nhau làm bạn bè
+    const senderReceivedRequest = sender.friendRequests.find(request => String(request.sender) === String(receiverID));
+    if (senderReceivedRequest) {
+      sender.friends.push(receiverID);
+      receiver.friends.push(senderID);
+
+      // Xóa lời mời từ mảng friendRequests cả hai
+      sender.friendRequests = sender.friendRequests.filter(request => String(request.sender) !== String(receiverID));
+      receiver.friendRequests = receiver.friendRequests.filter(request => String(request.sender) !== String(senderID));
+    } else {
+      // Thêm lời mời vào mảng friendRequests
+      receiver.friendRequests.push({ sender: senderID, status: 'pending' });
+    }
+
+    // Lưu cập nhật vào cơ sở dữ liệu
+    await receiver.save();
+    await sender.save();
+
+    res.json({ status: 1, message: 'Đã gửi lời mời kết bạn' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ status: 0, message: 'Lỗi khi gửi lời mời kết bạn' });
+  }
+});
+
+
+
+// Chấp nhận lời mời kết bạn
+router.post('/accept-friend-request', async (req, res) => {
+  try {
+    const { userID, senderID } = req.body;
+
+    const user = await User.findById(userID);
+    const sender = await User.findById(senderID);
+
+    if (!user || !sender) {
+      return res.status(404).json({ status: 0, message: 'Người dùng không tồn tại' });
+    }
+
+    // Kiểm tra xem lời mời còn tồn tại không
+    const friendRequestIndex = user.friendRequests.findIndex(request => String(request.sender) === String(senderID));
+    if (friendRequestIndex === -1) {
+      return res.status(400).json({ status: 0, message: 'Lời mời kết bạn không tồn tại' });
+    }
+
+    // Thêm người gửi vào mảng friends của cả hai người dùng
+    user.friends.push(senderID);
+    sender.friends.push(userID);
+
+    // Xóa lời mời từ mảng friendRequests
+    user.friendRequests.splice(friendRequestIndex, 1);
 
     // Lưu cập nhật vào cơ sở dữ liệu
     await user.save();
-    await friend.save();
+    await sender.save();
 
-    res.json({ status: 1, message: 'Kết bạn thành công' });
+    res.json({ status: 1, message: 'Chấp nhận lời mời kết bạn thành công' });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ status: 0, message: 'Lỗi khi kết bạn' });
+    res.status(500).json({ status: 0, message: 'Lỗi khi chấp nhận lời mời kết bạn' });
   }
 });
+// Từ chối lời mời kết bạn
+router.post('/reject-friend-request', async (req, res) => {
+  try {
+    const { userID, senderID } = req.body;
+
+    const user = await User.findById(userID);
+
+    if (!user) {
+      return res.status(404).json({ status: 0, message: 'Người dùng không tồn tại' });
+    }
+
+    // Kiểm tra xem lời mời còn tồn tại không
+    const friendRequestIndex = user.friendRequests.findIndex(request => String(request.sender) === String(senderID));
+    if (friendRequestIndex === -1) {
+      return res.status(400).json({ status: 0, message: 'Lời mời kết bạn không tồn tại' });
+    }
+
+    // Xóa lời mời từ mảng friendRequests
+    user.friendRequests.splice(friendRequestIndex, 1);
+
+    // Lưu cập nhật vào cơ sở dữ liệu
+    await user.save();
+
+    res.json({ status: 1, message: 'Từ chối lời mời kết bạn thành công' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ status: 0, message: 'Lỗi khi từ chối lời mời kết bạn' });
+  }
+});
+
+
 
 module.exports = router;
