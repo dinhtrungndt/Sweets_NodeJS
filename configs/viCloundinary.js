e0ab3aa40057d09c1dc15d9c78151787d08e7e14
Mang Tuấn Vĩ <tuanvimang@gmail.com>
2024-02-19 09:16:28 +0700

    update

---
+++ configs/viCloundinary.js
@@ -0,0 +1,8 @@
+import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({ 
+  cloud_name: 'dfh1x8dtw', 
+  api_key: '182242976743445', 
+  api_secret: 'AtujP-65GcmIXf2cZcT0iI9T5_I' 
+});
+export default cloudinary;
